3002d149d3108a969a49764e816fb9e46c0d0f27
Ahmet Coskuner <acoskuner500@gmail.com>
2022-10-17 00:09:30 -0500

    missing commits

--- lab5_htax_sequence/lab5/tb/htax_seqs.sv
+++ lab5_htax_sequence/lab5/tb/htax_seqs.sv
@@ -49,12 +49,100 @@ endclass : fix_dest_port_seq
 
 //TO DO - Sequence 2
 //Short Packet Sequence extends from base sequence
+class short_packet_seq extends htax_base_seq;
+
+	//Factory Registration
+	`uvm_object_utils(short_packet_seq)
+
+	//Constructor
+	function new ( string name = "short_packet_seq");
+    super.new(name);
+  endfunction : new
+
+	//Body task -- 
+	virtual task body();
+		int i;
+		i=$urandom_range(0,3);
+		`uvm_info(get_type_name(),"Executing fix destination port sequence with 5 transactions", UVM_NONE)
+		//Generate a sequence with 5 packets
+		repeat(5) begin
+			`uvm_do_with (req, {req.length inside {[3:10]};})
+		end
+	endtask
+
+endclass : short_packet_seq
 
 //TO DO - Sequence 3
 //Long Packet Short Delay Sequence extends from base sequence
+class long_packet_short_delay_seq extends htax_base_seq;
+
+	//Factory Registration
+	`uvm_object_utils(long_packet_short_delay_seq)
+
+	//Constructor
+	function new ( string name = "long_packet_short_delay_seq");
+    super.new(name);
+  endfunction : new
+
+	//Body task -- 
+	virtual task body();
+		int i;
+		i=$urandom_range(0,3);
+		`uvm_info(get_type_name(),"Executing fix destination port sequence with 5 transactions", UVM_NONE)
+		//Generate a sequence with 5 packets
+		repeat(5) begin
+			`uvm_do_with (req, {req.length inside {[40:50]}; req.delay < 5;})
+		end
+	endtask
+
+endclass : long_packet_short_delay_seq
 
 //TO DO - Sequence 4
 //Medium Packet fix VC Sequence extends from base sequence
+class med_packet_fixed_vc_seq extends htax_base_seq;
+
+	//Factory Registration
+	`uvm_object_utils(med_packet_fixed_vc_seq)
+
+	//Constructor
+	function new ( string name = "med_packet_fixed_vc_seq");
+    super.new(name);
+  endfunction : new
+
+	//Body task -- 
+	virtual task body();
+		int i;
+		i=$urandom_range(0,3);
+		`uvm_info(get_type_name(),"Executing fix destination port sequence with 5 transactions", UVM_NONE)
+		//Generate a sequence with 5 packets
+		repeat(5) begin
+			`uvm_do_with (req, {req.length inside {[10:40]}; req.vc==1;})
+		end
+	endtask
+
+endclass : med_packet_fixed_vc_seq
 
 //TO DO - Sequence 5
 //True Random VC Sequence extends from base sequence
+class random_seq extends htax_base_seq;
+
+	//Factory Registration
+	`uvm_object_utils(random_seq)
+
+	//Constructor
+	function new ( string name = "random_seq");
+    super.new(name);
+  endfunction : new
+
+	//Body task -- 
+	virtual task body();
+		int i;
+		i=$urandom_range(0,3);
+		`uvm_info(get_type_name(),"Executing fix destination port sequence with 5 transactions", UVM_NONE)
+		//Generate a sequence with 5 packets
+		repeat(5) begin
+			`uvm_do_with (req, {}) 
+		end
+	endtask
+
+endclass : random_seq
